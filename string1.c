5477d60c18dc4b491759645b8bfa8058484d1f03
Ali <alierrami2024@gmail.com>
2023-08-21 04:42:17 -0700

    Hard work today

---
+++ string1.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+/**
+ * _putchar - writes the character to stdout
+ * @c: the character to print
+ * Return: on success 1
+ * 	   on error: -1 is returned and error is set appropriately
+ */
+int _putchar(char c)
+{
