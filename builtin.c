1f0cfa1034aad77d9c67a9c330da049b4a17f195
Ali <alierrami2024@gmail.com>
2023-08-23 15:25:52 -0700

    Good job

--- builtin.c
+++ builtin.c
@@ -4,4 +4,13 @@
  * @info: structure containing potential arguments
  * Return: always 0
  */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
 
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		-puts(*arg_array);
+	return (0);
+}
