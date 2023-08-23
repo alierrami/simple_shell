e4d3202b56c83260fdf2c104b56ee61bcb122bf1
Ali <alierrami2024@gmail.com>
2023-08-23 13:27:13 -0700

    New work

--- builtin1.c
+++ builtin1.c
@@ -4,3 +4,19 @@
  * @node: the alias node
  * Return: always 0 on success or 1 on error
  */
+int print_alias(list_t *node)
+{
+	char *p = NULL, *a = NULL;
+
+	if (node)
+	{
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+			_putchar(*a);
+		_putchar('\'');
+		_puts(p + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
